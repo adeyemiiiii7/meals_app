7c781c0d0bc633e2a94fc2c2ef2cb5d9dbfa2cca
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-06-19 23:52:23 -0400

    modified:   lib/categories.dart 	modified:   lib/main.dart

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_grid_item.dart';
+import 'package:meals_app/data/dummy_dart.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,42 +13,15 @@ class CategoriesScreen extends StatelessWidget {
           title: const Text("Pick your category"),
         ),
         body: GridView(
+          padding: EdgeInsets.all(24),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
-              childAspectRatio: 1.5,
+              childAspectRatio: 1.75,
               crossAxisSpacing: 20,
               mainAxisSpacing: 20),
-          children: const [
-            Text(
-              '1',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              '1',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              '1',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              '1',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              '1',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(category: category)
           ],
         ));
   }
