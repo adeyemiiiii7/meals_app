5eaeb9a0c9d8addc2e40b1b8f8d10966a06a6f23
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-06-26 03:01:48 -0400

    new changes

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../models/meal.dart';
 
@@ -7,17 +8,150 @@ class MealDetails extends StatelessWidget {
 
   final Meal meal;
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(meal.title),
+//       ),
+//       body: ListView(
+//         children: [
+//           Image.network(
+//             meal.imageUrl,
+//             height: 300,
+//             width: double.infinity,
+//             fit: BoxFit.cover,
+//           ),
+//           const SizedBox(height: 14),
+//           Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 16),
+//             child: Text(
+//               'Ingredients',
+//               style: Theme.of(context).textTheme.titleLarge!.copyWith(
+//                     color: Theme.of(context).colorScheme.primary,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//             ),
+//           ),
+//           const SizedBox(height: 14),
+//           for (final ingredient in meal.ingredients)
+//             ListTile(
+//               leading: Icon(Icons.circle,
+//                   color: Theme.of(context).colorScheme.primary),
+//               title: Text(
+//                 ingredient,
+//                 style: TextStyle(
+//                     color: Theme.of(context).colorScheme.onBackground),
+//               ),
+//             ),
+//           const SizedBox(height: 18),
+//           Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 16),
+//             child: Text(
+//               'Steps',
+//               style: Theme.of(context).textTheme.titleLarge!.copyWith(
+//                     color: Theme.of(context).colorScheme.primary,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//             ),
+//           ),
+//           const SizedBox(height: 14),
+//           for (final step in meal.steps)
+//             Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+//               child: ListTile(
+//                 leading: Icon(Icons.circle,
+//                     color: Theme.of(context).colorScheme.primary),
+//                 title: Text(
+//                   step,
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                       color: Theme.of(context).colorScheme.onBackground),
+//                 ),
+//               ),
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+// }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
+      body: ListView(
+        children: [
+          Image.network(
+            meal.imageUrl,
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 14),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 14),
+          for (final ingredient in meal.ingredients)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '• ',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Expanded(
+                    child: Text(
+                      ingredient,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          const SizedBox(height: 18),
+          Text(
+            'Steps',
+            textAlign: TextAlign.justify,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 14),
+          for (int i = 0; i < meal.steps.length; i++)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 4, 8, 4),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${i + 1}. ',
+                    style: GoogleFonts.poppins(
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      meal.steps[i],
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
       ),
     );
   }
