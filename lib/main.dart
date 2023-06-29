358c60c88214733d69d050461178d402a0f3a213
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-06-28 21:46:23 -0400

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/screens/categories.dart';
+//import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/tabs.dart';
 //import 'package:meals_app/screens/meals.dart';
 
@@ -11,7 +10,7 @@ final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(120, 109, 177, 246),
+    seedColor: const Color.fromARGB(120, 26, 126, 225),
   ),
   textTheme: GoogleFonts.poppinsTextTheme(),
 );
@@ -25,13 +24,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: theme,
-      home: const TabsScreen(),
-      // MealsScreen(
-      //   title: 'Some Category',
-      //   meals: dummyMeals,
-      // ),
-    );
+        debugShowCheckedModeBanner: false, theme: theme, home: categ
+        // const TabsScreen(),
+        // MealsScreen(
+        //   title: 'Some Category',
+        //   meals: dummyMeals,
+        // ),
+        );
   }
 }
