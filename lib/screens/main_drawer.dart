9fe3040537bc3c54a7dd67078978e6dafc5d6de8
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-07-01 22:31:19 -0400

    new changes

--- lib/screens/main_drawer.dart
+++ lib/screens/main_drawer.dart
@@ -76,7 +76,7 @@ class MainDrawer extends StatelessWidget {
                   ),
             ),
             onTap: () {
-              onSelectScreen('Filters');
+              onSelectScreen('filters');
             },
           )
         ],
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
-import 'package:meals_app/widgets/main_drawer.dart';
+import 'package:meals_app/screens/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -48,13 +49,16 @@ class _TabsScreenState extends State<TabsScreen> {
   }
 
   void _setScreen(String identifier) {
+    Navigator.of(context).pop();
     if (identifier == 'filters') {
-    } else {
-      //Navigator.pop(context).pop();
-      Navigator.pop(context).pop();
-      return;
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
+      );
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
