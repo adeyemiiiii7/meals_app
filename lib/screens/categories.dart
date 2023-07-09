9edf55a0801179a2f62089c16fa4a35c0bb3d468
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-07-08 23:23:50 -0400

    modified:   lib/screens/categories.dart

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -7,16 +7,46 @@ import '../models/category.dart';
 import '../models/meal.dart';
 //import '../models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen(
       {super.key,
       //  required this.onToggleFavourite,
       required this.avaliableMeals});
 
   final List<Meal> avaliableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+// State objects in Flutter represent the current state of a widget.
+// They hold data that can change over time and trigger a rebuild of the UI when the data changes.
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  //extend with SingleTickerProviderStateMixin >= for only one animation controller
+  //extend with TickerProviderStateMixin >= for multiple animation x=controller
+  //your animation must be working with your sate object because it works behind the scene
   // final void Function(Meal meal) onToggleFavourite;
+
+  late AnimationController _animatorController;
+  void initState() {
+    super.initState();
+    _animatorController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animatorController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final selectedMeals = avaliableMeals
+    final selectedMeals = widget.avaliableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
